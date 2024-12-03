fd32cdce7d0a3b993f5a3afb86e6a4b46f2e02cd
CarlosArgueta <carguetar@unah.hn>
2024-12-03 11:08:55 -0600

    Datos del Anuncio de Visita a Pantalla Visita Creada

--- lib/pages/visita.dart
+++ lib/pages/visita.dart
@@ -1,17 +1,43 @@
 import "package:flutter/material.dart";
-
 class VisitaCreada extends StatelessWidget {
-  const VisitaCreada ({super.key});
+  final String nombre;
+  final String apellido;
+  final String identidad;
+  final String motivo;
+  final String fecha;
+  final String hora;
+
+  const VisitaCreada({
+    super.key,
+    required this.nombre,
+    required this.apellido,
+    required this.identidad,
+    required this.motivo,
+    required this.fecha,
+    required this.hora,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Visita creada'),
+        backgroundColor: Colors.orange,
       ),
-      body: const Center(
-        child: Text('Visita creada'),
-      )
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Nombre: $nombre', style: const TextStyle(fontSize: 18)),
+            Text('Apellido: $apellido', style: const TextStyle(fontSize: 18)),
+            Text('DNI: $identidad', style: const TextStyle(fontSize: 18)),
+            Text('Motivo: $motivo', style: const TextStyle(fontSize: 18)),
+            Text('Fecha: $fecha', style: const TextStyle(fontSize: 18)),
+            Text('Hora: $hora', style: const TextStyle(fontSize: 18)),
+          ],
+        ),
+      ),
     );
   }
 }
