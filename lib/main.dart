ea9166feac0a8823317aa450d366c9da2596a479
omarpadilla1472 <omarpadilla1472@gmail.com>
2024-12-07 03:10:03 +0000

    more firebase stuff- WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/pages/principal.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-
-
-
-void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
+    return MaterialApp(
         title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      onGenerateRoute: (settings) {
-        return null;
-      }
-      );
-      
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+        onGenerateRoute: (settings) {
+          return null;
+        });
   }
 }
